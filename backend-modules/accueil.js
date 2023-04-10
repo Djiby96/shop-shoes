6688d383178c6e039689d952b307116272256298
Djiby96 <sarrdjiby20@gmail.com>
2023-04-10 15:59:23 +0000

    LATEST COMMIT

--- backend-modules/accueil.js
+++ backend-modules/accueil.js
@@ -2,7 +2,8 @@
     1. Afficher la page d'accueil
 */
 
-var con = require("./MySQL.js")
+var con = require("./MySQL.js"),
+    fs = require("fs");
 
 var Accueil = {
     // 1
@@ -10,17 +11,27 @@ var Accueil = {
         var R_PRODUCTS = `SELECT chemin, titre, prix, numero, categorie, genre, type, marque, status, date, REPLACE(FORMAT(prix, 0), ',', ' ') AS 'prix_format', REPLACE(titre, ' ', '-') AS 'titre_concat' FROM produit INNER JOIN images USING(numero) ` + 
                          `WHERE status='en stock' ORDER BY date DESC LIMIT 0, 10`;
 
+
+        var data = fs.readFileSync("./backend-modules/h_data.js", "utf-8");
+        var p = JSON.parse(data);
+
+        res.render("accueil.html", {
+            products: p
+        });                
+
         //select data  
-        con.query(R_PRODUCTS, (err, rs) =>{
-            if(err){
-                var products = []
-            }else{
-                var products = rs;
-            }
-            res.render("accueil.html", {
-                products: products
-            });
-        })
+        // con.query(R_PRODUCTS, (err, rs) =>{
+        //     if(err){
+        //         var products = []
+        //     }else{
+        //         var products = rs;
+        //         var d = JSON.stringify(products);
+        //         fs.writeFileSync("./backend-modules/h_data.js", d)
+        //     }
+        //     res.render("accueil.html", {
+        //         products: products
+        //     });
+        // })
     }
 }
 
